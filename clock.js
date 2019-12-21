b6ce5a58cc9280522daa9bbf2cdbeac2d449d609
ykarr <38881079+ykarr@users.noreply.github.com>
2019-12-22 02:04:26 +0900

    수정중

--- clock.js
+++ clock.js
@@ -73,13 +73,4 @@ function toggleModal() {
    $(this).removeClass("hover");
  }
  );
- /*댓글*/
- //댓글 자바스크립트
- (function(d, s) {
-     var j, e = d.getElementsByTagName(s)[0];
-     if (typeof LivereTower === 'function') { return; }
-     j = d.createElement(s);
-     j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-     j.async = true;
-     e.parentNode.insertBefore(j, e);
- })(document, 'script');
+ 
